d05abec80d8cf187a02100ae1dc9f1422b1b0a42
Michał Kaszlej <mkaszlej@gmail.com>
2014-10-28 13:27:33 +0100

    [TASK] Stripped unnecessary code

--- GameLayer.h
+++ GameLayer.h
@@ -4,46 +4,13 @@
 #include "Box2D/Box2D.h"
 #include "cocos2d.h"
 #include "Ground.h"
-#include "Cannon.h"
-#include "Bullet.h"
-#include "Shrapnel.h"
 
 using namespace cocos2d;
-
-class BulletListener : public b2ContactListener
-{
-	
-	void BeginContact(b2Contact* contact) {
-   
-		if(!contact->IsTouching()) return;
-   
-		//check if fixture A was a projectile
-		void* bodyUserData = contact->GetFixtureA()->GetBody()->GetUserData();
-		if ( bodyUserData )
-		{
-			if( strcmp( static_cast<GameObject*>( bodyUserData )->name->c_str() , "BULLET" ) == 0 )
-				static_cast<Bullet*>( bodyUserData )->markedToExplode = true;
-		}
-			
-		//check if fixture B was a ball
-		bodyUserData = contact->GetFixtureB()->GetBody()->GetUserData();
-		if ( bodyUserData )
-		{
-			if( strcmp( static_cast<GameObject*>( bodyUserData )->name->c_str() , "BULLET" )  == 0 )
-				static_cast<Bullet*>( bodyUserData )->markedToExplode = true;
-		}		
-    }
-};
-
-BulletListener bulletListener;
 
 class GameLayer : public cocos2d::Layer
 {
 		
 private:
-
-	Sprite * bodySprite;
-	b2Body * body;
 
 	//texture data
 	int nStripes;
@@ -55,15 +22,10 @@ private:
 
     b2World * m_world;
 
-	Cannon * cannon;
-
-	b2Vec2 clickedPoint;
-
-	std::vector<Bullet*> bullets;
-
 public:
 
 	CustomCommand drawStripesCommand;
+
 	Sprite * noiseSprite;
 	Sprite * stripes;
 
@@ -89,17 +51,11 @@ public:
 		//Generate Ground
 		ground = new Ground(this, m_world);
 		ground->generateHills();
-		
-		cannon = NULL;
-		body = NULL;
-		bodySprite = NULL;
-		clickedPoint = b2Vec2 ( 100 , 10 );
-		
+		genBackground();
+				
 		this->setTouchEnabled(true);
 		
 		this->schedule( schedule_selector(GameLayer::tick) );
-	  	
-	  	this->setScale( 1 );
 	  	
 		return true;
 	}
@@ -109,8 +65,7 @@ public:
 		b2Vec2 gravity;
 		gravity.Set( 0.0f , -10.0f );
 		m_world = new b2World( gravity );		
-
-		m_world->SetContactListener(&bulletListener);
+	
 	}
 	
 	Texture2D * createTexture( Color4F bgColor, float textureWidth, float textureHeight, int nStripes ){
@@ -283,79 +238,13 @@ public:
 		stripes->retain();
 	}
  
- 	void createSky(){
-		Color4F bgColor = randomBrightColor();
-		Texture2D * texture = createTexture(bgColor, 512.0 , 512.0 , 6);
-		Sprite * sky = Sprite::createWithTexture( texture );
-		sky->setPosition( winSize.width/2, winSize.height/2 );
-		cocos2d::Texture2D::TexParams params = {GL_LINEAR, GL_LINEAR, GL_REPEAT,  GL_REPEAT};
-		sky->getTexture()->setTexParameters(params);
-
-		this->addChild(sky,20);
-	}
-	
-	void createBodyAtPoint(b2Vec2 point)
-	{
-		
-		if( body != NULL ){
-			m_world->DestroyBody( body );
-			body = NULL;
-		}
-		
-		if( bodySprite != NULL )
-		{
-			this->removeChild( bodySprite );
-			bodySprite = NULL;
-		}
-		
-		b2BodyDef def;
-		def.type = b2_dynamicBody;
-		def.position.Set( point.x , point.y );
-		
-		b2FixtureDef fixture;
-		
-		b2CircleShape shape;
-
-		shape.m_radius = 2; 
-			
-		body = m_world->CreateBody( &def );
-			
-		fixture.density = 1;
-		fixture.shape = &shape;
-		//fixture.filter.categoryBits = BULLET;
-		//fixture.filter.maskBits = GROUND | CANNON;
-		
-		body->CreateFixture(&fixture);
-			
-		bodySprite = Sprite::create("kolo.png",  cocos2d::CCRectMake(0, 0, 256, 256));
-		bodySprite->setScale(0.5);
-		bodySprite->setPosition( Vec2( ( point.x )*PTM_RATIO  , ( point.x )*PTM_RATIO  ) );
-		
-		this->addChild( bodySprite , 0 );
-		 
-	}
-	
-	void createBullet(){
-
-		Bullet * bullet = new Bullet(this, m_world, 0.5 , 1);
-		bullet->createBullet( cannon->GetBarrelExit() );
-		bullet->shoot( cannon->GetShootingDirection() , 2 );
-		
-		bullets.push_back(bullet);
-
-	}
-	
 	void onTouchesMoved(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event* event)
 	{
 		for( auto touch : touches )
 		{
-			// Choose one of the touches to work with
 			Point location = touch->getLocationInView();
 			location = Director::sharedDirector()->convertToGL(location);
 
-			if( location.x > 300 && location.x < 800 ){	
-				clickedPoint = b2Vec2( location.x/PTM_RATIO , location.y/PTM_RATIO );
-			}
 		}		
 	}
 
@@ -366,31 +255,6 @@ public:
 		Point location = touches[0]->getLocationInView();
 		location = Director::sharedDirector()->convertToGL(location);
 		
-		if( location.x < 150 )
-		{	
-			
-			if( ground != NULL )
-			{
-				genBackground();
-				ground->generateHills();
-			}
-			
-			if( cannon != NULL )
-			{
-				delete cannon;
-			}
-			
-			//Add cannon
-			cannon = new Cannon(this, m_world);
-			cannon->createCannon( b2Vec2( location.x/PTM_RATIO , location.y/PTM_RATIO ) );
-		}
-		
-		if( location.x > 800 )
-		{
-			cannon->shoot();
-			createBullet();
-		}
-		
 	}
 
 	void tick(float dt){
@@ -398,53 +262,14 @@ public:
 		int velocityIterations = 8;
 		int positionIterations = 1;
 		
-		static float offsetCannon = 0;
-		
 		m_world->Step(dt, velocityIterations, positionIterations);
 				
 				
-		if( cannon != NULL )
-		{
-			cannon->rotateBarrel( clickedPoint );
-			cannon->updateSprites();
-			offsetCannon = cannon->getMainSprite()->getPosition().x;
-		}
-		
-		//Explode bullets
-		for(int i = 0; i < bullets.size(); i++)
-		{
-			Bullet * bullet = bullets[i];
-			if( !bullet->updateSprites() )
-			{
-				bullets.erase(bullets.begin()+i);
-				delete bullet;
-			}
-			else{
-				offsetCannon = bullet->getMainSprite()->getPosition().x;
-			}
-		}
-		
-		if( bodySprite != NULL )
-		{
-			bodySprite->setPosition( Point( body->GetPosition().x * PTM_RATIO, body->GetPosition().y * PTM_RATIO) );
-			bodySprite->setRotation( -1 * CC_RADIANS_TO_DEGREES( body->GetAngle()) );
-		}
-						
-		float PIXELS_PER_SECOND = 100;
-		static float offset = 0;
-		offset += PIXELS_PER_SECOND * dt;
-  
-		if( ground != NULL )
-		{
-			ground->setOffsetXTexture( offset );
-			ground->setOffsetX( offsetCannon );
+		if( ground != NULL ){
 			ground->update();
 		}
 		
-		
 	}
-
-
 
 };
 
